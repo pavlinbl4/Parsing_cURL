1afed2d46f40df5bb2c20dd172d230503913eb2d
evgeniy <pavlinbl4@gmail.com>
2023-10-15 18:09:57 +0300

    clear author from TASS caption

---
+++ tools/clear_caption.py
@@ -0,0 +1,12 @@
+import re
+
+
+def clear_author(text_str):
+    pattern = r"Фото ИТАР-ТАСС/ Семен Лиходеев"
+    return re.sub(pattern, '', text_str)
+
+
+if __name__ == '__main__':
+    text_str = 'Портретура Россия. Москва. Актриса Инна Чурикова во время пресс-конференции артистов театра "Ленком" в ДК "Выборгский". Фото ИТАР-ТАСС/ Семен Лиходеев'
+
+    print(clear_author(text_str))
