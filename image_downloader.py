218b193451aaa2b5205f4cb88c5e7f42fe43fb37
evgeniy <pavlinbl4@gmail.com>
2023-11-13 13:09:33 +0300

    download image file with its origanal file name

---
+++ image_downloader.py
@@ -0,0 +1,31 @@
+import requests
+
+from image_name_with_sales_count import add_sales_count_to_image_name
+from origanal_file_name import extract_image_name
+from tools.create_sub_directory import create_directory
+
+
+def download_jpeg_image(image_link, sales_count):
+
+    # create folder for images in root
+    image_folder = create_directory(".", 'Images')
+
+    # original file name from site
+    image_name = extract_image_name(image_link)
+
+    # add information about sales to file -  optional
+    image_name_sales = add_sales_count_to_image_name(image_name, sales_count)
+
+    try:
+        r = requests.get(image_link, stream=True)
+        with open(
+                f'{image_folder}/{image_name_sales}',
+                "bw") as f:
+            for chunk in r.iter_content(9000):
+                f.write(chunk)
+    except Exception as ex:
+        print(ex)
+
+
+if __name__ == '__main__':
+    download_jpeg_image("https://iv.kommersant.ru/relay/relayimage/1653806tyucolon20230929154700/200", 888)
