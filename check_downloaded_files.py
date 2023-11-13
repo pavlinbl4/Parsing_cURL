62f375ca1d91c5f0bb5e110cdabb61356e042a5f
evgeniy <pavlinbl4@gmail.com>
2023-11-13 13:39:29 +0300

    add function to download prevue^ optimized enumerate cycle

--- check_downloaded_files.py
+++ check_downloaded_files.py
@@ -1,33 +1,38 @@
+from image_downloader import download_jpeg_image
 from tools.files_work import find_files
 from tools.regex_tools import images_number_int
 from tools.soup import make_soup_from_offline_file
 from icecream import ic
+from tqdm import trange, tqdm
 
 
-def get_kommersant_data(offline_html):
+def get_kommersant_data(offline_html, count: int) -> int:
     soup = make_soup_from_offline_file(offline_html)
-
-    #  table with all images  -  class="ps_mosaic__items"
     all_thumbs_data = soup.find_all('div', class_='ps_mosaic__items')
-
     thumb_data = all_thumbs_data[0].find_all('div', class_="ps_mosaic__item")
-
+    sales_on_one_file = 0
     for image in thumb_data:
         sales_number = image.find(class_="ps_sales ps_sales--green").text
-        if images_number_int(sales_number, pattern=r'(?<=продаж:\s)\d+') != 0:
-            print(sales_number)
-            print(image.find(class_="ps_mosaic__item_text").text)
-            print(image.find('img', class_="ps_lenta__image").get('src'))
+        sales_number_int = images_number_int(sales_number, pattern=r'(?<=продаж:\s)\d+')
+        if sales_number_int != 0:
+            sales_on_one_file += sales_number_int
+            # count += sales_number_int
+            # print(sales_number)
+            # print(image.find(class_="ps_mosaic__item_text").text)
+            image_link = image.find('img', class_="ps_lenta__image").get('src')
+            download_jpeg_image(image_link, sales_number_int)
+    count += sales_on_one_file
+    return count
 
 
 def main_check_downloaded_files(dir_path: str):
-    ic(dir_path)
     all_html_files = find_files(dir_path, 'html')
-    for number, file in enumerate(all_html_files):
-        print(f'{number} rezult_{number + 1}.html\n{"*" * 200}')
-        get_kommersant_data(f'{dir_path}/{file}')
+    count = 0
+    for number, file in tqdm(enumerate(all_html_files, start=1)):
+        count = get_kommersant_data(f'{dir_path}/rezult_{number}.html', count)
+    print(f'Sales on all pages{count = }')
 
 
 if __name__ == '__main__':
     main_check_downloaded_files('./kommersant')
-    # get_kommersant_data('./kommersant/rezult_2.html')
+    # get_kommersant_data('./kommersant/rezult_2.html', 0)
