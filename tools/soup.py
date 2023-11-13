d1f5f7cffe0882dd3c26e542f7124c3ddf21a780
evgeniy <pavlinbl4@gmail.com>
2023-11-13 14:24:20 +0300

    soup tools

--- tools/soup.py
+++ tools/soup.py
@@ -2,9 +2,17 @@
 
 from bs4 import BeautifulSoup
 
+from tools.read_html import read_html
+
 
 def get_soup(html):
     return BeautifulSoup(html, 'lxml')
+
+
+def make_soup_from_offline_file(offline_html):
+    offline_file = read_html(offline_html)
+    soup = get_soup(offline_file)
+    return soup
 
 
 def get_html(link, browser):
