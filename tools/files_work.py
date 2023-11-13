e28c65789faf4818010d43f92ea66c43e9db7c3c
evgeniy <pavlinbl4@gmail.com>
2023-11-13 14:26:25 +0300

    refactoring

--- tools/files_work.py
+++ tools/files_work.py
@@ -1,7 +1,8 @@
 import os
+from icecream import ic
 
 
-def find_files(dir_path:str, extension:str)-> list:  # find all files with extension in directory
+def find_files(dir_path: str, extension: str) -> list:  # find all files with extension in directory
     return [f for f in os.listdir(dir_path) if f.endswith(extension)]
 
 
@@ -10,3 +11,8 @@ def file_name_and_dir_path(full_path: str):
     dir_path = '/'.join(full_path.split('/')[:-1])
     file_name = full_path.split('/')[-1]
     return file_name, dir_path
+
+
+if __name__ == '__main__':
+    ic(find_files('../kommersant', 'html'))
+    # ic(find_files('../tass_html', 'html'))
